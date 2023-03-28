8697b1431378455da99159397ecf755aa3fb5e5a
Maor <67946243+Maor-R@users.noreply.github.com>
2023-03-28 18:15:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import morgan from 'morgan';
+import {Server} from 'socket.io';
 
 
 import errorHandler from './middleware/errorHandler.js';
@@ -33,6 +34,28 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
+const io = new Server( {
+  cors: {
+    origins: '*:*'  }
+});
+
+
+io.listen(5001);
+
+io.on('connection', (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  // console.log(socket.rooms);
+  socket.on('newUser', (data, fn) => {
+
+  });
+  socket.on('join', function(data) {
+    socket.join(data.room);
+});
+socket.on('room1', function(data) {
+  socket.join(data.room);
+  io.to('room1').emit('chat message', data.msg);
+});
+  
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`.red);
